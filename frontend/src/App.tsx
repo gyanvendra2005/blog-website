e67e86d871068017373b01b3def32a48dcabe893
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-10-27 13:16:19 +0530

    done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,40 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Signup } from './pages/Signup'
-import { Signin } from './pages/Signin'
-import { Blog } from './pages/Blog'
+import { createBrowserRouter, RouterProvider} from 'react-router-dom'
 import { Blogs } from './pages/Blogs'
+import Layout from './Layout'
+import { LoginComponent } from './components/LoginComponent'
 
 function App() {
 
+  const Router = createBrowserRouter([
+    {
+       path:'/',
+       element:<Layout/>,
+       children:[
+        {
+          path:'',
+          element:<Blogs/>
+        },
+        {
+          path:'signup',
+          element:<LoginComponent type={'signin'} />
+        },
+       ]
+    }
+  ])
+
   return (
+
+    
     <>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
           <Route path='/blogs' element={<Blogs />} />
         </Routes>
-      </BrowserRouter>
+      </BrowserRouter> */}
+      <RouterProvider router={Router} />
     </>
   )
 }
