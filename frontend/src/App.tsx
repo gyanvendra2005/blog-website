d9d3e5ad5619b3f61e8df4fa87889e6c0dc9db12
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-10-27 13:10:40 +0530

    headers set

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,21 @@
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './pages/Signup'
+import { Signin } from './pages/Signin'
+import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
-import Layout from './Layout'
-import { LoginComponent } from './components/LoginComponent'
 
 function App() {
-//   <BrowserRouter>
-//   <Routes>
-//     <Route path="/signup" element={<Signup />} />
-//     <Route path="/signin" element={<Signin />} />
-//     <Route path="/blog" element={<Blog />} />
-//     <Route path="/blogs" element={<Blogs />} />
-//   </Routes>
-// </BrowserRouter>
-
-const Router = createBrowserRouter([
-  {
-     path:'/',
-     element:<Layout/>,
-     children:[
-      {
-        path:'',
-        element:<Blogs/>
-      },
-      {
-        path:'signup',
-        element:<LoginComponent type={'signin'} />
-      },
-     ]
-  }
-])
 
   return (
     <>
-       <RouterProvider router={Router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path='/blogs' element={<Blogs />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
