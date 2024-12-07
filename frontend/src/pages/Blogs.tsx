e7def703349366317bc21fe281bfbb53cf31e285
Gyanvendra Singh <gyanvendras2004@gmail.com>
2024-12-07 21:49:47 +0530

    public blogs pages updated

--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -18,18 +18,18 @@ interface Blog{
 
 export const Blogs = () => {
       
-    const token = localStorage.getItem("token")
-    if(!token){
-        alert("YOU ARE NOT SIGN IN")
-    }
+    // const token = localStorage.getItem("token")
+    // if(!token){
+    //     alert("YOU ARE NOT SIGN IN")
+    // }
 
     const [blogs, setBlogs] = useState<Blog[]>([]);
 
     useEffect(() => {
       axios.get(`${BACKEND_URL}/api/v1/blogs/bulk`,{
-        headers:{
-            Authorization:localStorage.getItem("token")
-        }
+        // headers:{
+        //     Authorization:localStorage.getItem("token")
+        // }
       })
       .then(response => {
         setBlogs(response.data.posts);
@@ -42,7 +42,7 @@ export const Blogs = () => {
         
         <div className="flex justify-center">
         <div className="flex flex-col items-center">
-            {token ? (
+            {
                 Array.isArray(blogs) && blogs.length > 0 ? (
                     blogs.map(blog => (
                         <BlogCard
@@ -59,24 +59,6 @@ export const Blogs = () => {
                         <Skeleton />
                         <Skeleton />
                     </div>
-                )
-            ) : (
-                <div className="flex flex-col h-screen items-center text-center">
-                    <img 
-                        src="/path/to/error-image.png" 
-                        alt="Not Authenticated" 
-                        className="w-32 h-32 mb-4" // Adjust size as needed
-                    />
-                    <p className="text-red-600 font-semibold">
-                        You are not authenticated.
-                    </p>
-                    <p className="mb-2 text-gray-700">
-                        Join us today to access exclusive content!
-                    </p>
-                    <button className="px-6 py-2 bg-blue-600 text-white font-semibold rounded-md hover:bg-blue-700 transition duration-200">
-                        Sign Up
-                    </button>
-                </div>
             )}
         </div>
     </div>
