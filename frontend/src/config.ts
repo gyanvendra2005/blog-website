8b9eeab12a59ff0cdb8375905bcb782b5d954d6e
Gyanvendra Singh <154894712+gyanvendra2005@users.noreply.github.com>
2024-12-08 00:16:09 +0530

    Update config.ts

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL =" https://backend.gyanvendra123456789.workers.dev"
-// export const BACKEND_URL ="https://backend.blogwebsite.workers.dev"
+// export const BACKEND_URL =" https://backend.gyanvendra123456789.workers.dev"
+export const BACKEND_URL ="https://backend.blogwebsite.workers.dev"
